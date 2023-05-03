dbe01425bc97f97bed030694c82901cd2a54c141
punk7535 <punk7535@gmail.com>
2023-05-03 15:24:37 +0800

    Add new file

---
+++ rand1.py
@@ -0,0 +1,9 @@
+import random # 載入random
+
+r1 = random.randint(1, 100)
+r2 = random.randint(1, 100)
+r3 = random.randint(1, 100)
+r4 = random.randint(1, 100)
+r5 = random.randint(1, 100)
+r6 = random.randint(1, 100)
+print(r1 , r2 , r3 , r4 , r5 , r6)
