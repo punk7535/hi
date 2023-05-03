91c4d55357ab0e61dc1eab0d2ff1c3cfc2d1c0f1
punk7535 <punk7535@gmail.com>
2023-05-03 11:12:28 +0800

    Add new file

---
+++ if3.py
@@ -0,0 +1,13 @@
+country = input('請問您是哪國人: ')
+age = input('請輸入年齡: ')
+age = int(age)
+if country == '台灣':
+	if age >= 18:
+		print('您可以考駕照')
+	else:
+		print('您不可以考駕照')
+elif country == '美國':
+	if age >= 16:
+		print('您可以考駕照')
+	else:
+		print('您不可以考駕照')
